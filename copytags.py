6516c55d96ad42163bb3bd2ba4081bd811a2f9d4
Ryan C. Thompson <rct@thompsonclan.org>
2016-10-29 20:39:37 -0700

    Major update to copytags
    - Eliminate QL as dependency (now only requires mutagen)
    - Use logging module instead of print()
    - Python 3 support (hopefully still supports Python 2)

--- copytags.py
+++ copytags.py
@@ -1,46 +1,77 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-import os, sys, re, UserDict
-from warnings import warn
-from itertools import *
-import quodlibet.config
-quodlibet.config.init()
-from quodlibet.formats import MusicFile
+import os, sys, re
+import logging
+# from itertools import *
+from mutagen import File as MusicFile
+from mutagen.aac import AACError
+from six.moves import map
 
-# config.init(os.path.join(os.getenv("HOME") + ".quodlibet" + "config"))
+try:
+    # Python 3
+    from collections.abc import MutableMapping
+except ImportError:
+    # Python 2
+    from UserDict import DictMixin as MutableMapping
 
-class AudioFile(UserDict.DictMixin):
+# Set up logging
+logFormatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.handlers = []
+logger.addHandler(logging.StreamHandler())
+for handler in logger.handlers:
+    handler.setFormatter(logFormatter)
+
+class AudioFile(MutableMapping):
     """A simple class just for tag editing.
 
     No internal mutagen tags are exposed, or filenames or anything. So
     calling clear() won't destroy the filename field or things like
     that. Use it like a dict, then .write() it to commit the changes.
+    When saving, tags that cannot be saved by the file format will be
+    skipped with a debug message, since this is a common occurrance
+    with MP3/M4A.
 
     Optional argument blacklist is a list of regexps matching
     non-transferrable tags. They will effectively be hidden, nether
     settable nor gettable.
 
-    Or grab the actual underlying quodlibet format object from the
-    .data field and get your hands dirty."""
-    def __init__(self, filename, blacklist=()):
-        self.data = MusicFile(filename)
+    Or grab the actual underlying mutagen format object from the
+    .data field and get your hands dirty.
+
+    """
+    def __init__(self, filename, blacklist=[], easy=True):
+        self.filename = filename
+        self.data = MusicFile(self.filename, easy=easy)
+        if self.data is None:
+            raise ValueError("Unable to identify %s as a music file" % (repr(filename)))
         # Also exclude mutagen's internal tags
         self.blacklist = [ re.compile("^~") ] + blacklist
     def __getitem__(self, item):
         if self.blacklisted(item):
-            warn("%s is a blacklisted key." % item)
+            logger.debug("Attempted to get blacklisted key: %s." % repr(item))
         else:
             return self.data.__getitem__(item)
     def __setitem__(self, item, value):
         if self.blacklisted(item):
-            warn("%s is a blacklisted key." % item)
+            logger.debug("Attempted to set blacklisted key: %s." % repr(item))
         else:
-            return self.data.__setitem__(item, value)
+            try:
+                return self.data.__setitem__(item, value)
+            except KeyError:
+                logger.debug("Skipping unsupported tag %s for file type %s",
+                             item, type(self.data))
     def __delitem__(self, item):
         if self.blacklisted(item):
-            warn("%s is a blacklisted key." % item)
+            logger.debug("Attempted to del blacklisted key: %s." % repr(item))
         else:
             return self.data.__delitem__(item)
+    def __len__(self):
+        return len(self.keys())
+    def __iter__(self):
+        return iter(self.keys())
+
     def blacklisted(self, item):
         """Return True if tag is blacklisted.
 
@@ -54,7 +85,7 @@ class AudioFile(UserDict.DictMixin):
     def keys(self):
         return [ key for key in self.data.keys() if not self.blacklisted(key) ]
     def write(self):
-        return self.data.write()
+        return self.data.save()
 
 # A list of regexps matching non-transferrable tags, like file format
 # info and replaygain info. This will not be transferred from source,
@@ -72,7 +103,7 @@ def substitute_prefix(path, oldprefix, newprefix):
     new_components = os.path.normpath(newprefix).split(os.sep)
     stripped_components = path_components[:len(old_components)]
     # Check to make sure path starts with oldprefix
-    for a,b in izip(old_components, stripped_components):
+    for a,b in zip(old_components, stripped_components):
         if a != b:
             raise Exception("path '%s' does not start with oldprefix '%s'" % (path, oldprefix))
     unstripped_components = path_components[len(old_components):]
@@ -96,7 +127,7 @@ def find_file_any_ext(path):
         base = os.path.splitext(p)[0]
         return base == target_base
     try:
-        return os.path.join(target_dir, ifilter(filter_fun, files).next())
+        return os.path.join(target_dir, next(iter(filter(filter_fun, files))))
     except StopIteration:
         raise Exception("Could not find a file with a basename of %s" % (path, ))
 
@@ -111,9 +142,9 @@ def unique (items, key_fun = None):
     If no key_fun is provided, then the identity function is assumed,
     in which case this is equivalent to list(set(items)).'''
     if key_fun is None:
-        return(list(set(items)))
+        return list(set(items))
     else:
-        return(dict([(key_fun(i), i) for i in items]).values())
+        return list({ key_fun(i): i for i in items }.values())
 
 def get_all_music_files (paths, ignore_hidden=True):
     '''Recursively search in one or more paths for music files.
@@ -138,42 +169,54 @@ def get_all_music_files (paths, ignore_hidden=True):
                 music_files.append(f)
 
     # Filter duplicate files and return
-    return sorted(unique(music_files, key_fun=lambda x: x['~filename']))
+    return sorted(unique(music_files, key_fun=lambda x: x.filename))
 
 def find_source_file(dest_file, src, dest):
     prefix_subbed = substitute_prefix(dest_file, dest, src)
     noext = os.path.splitext(prefix_subbed)[0]
     source_file = find_file_any_ext(noext)
     return source_file
-    # return find_file_any_ext(substitute_prefix(os.path.splitext(dest_file)[0],dest,src))
 
 def find_file_pairs(src,dest):
     """Returns a list of file pairs, each suitable for passing into copy_tags."""
-    dest_files = map(lambda f: f['~filename'], get_all_music_files(dest))
-    source_files = map(lambda f: find_source_file(f, src, dest), dest_files)
-    return izip(source_files, dest_files)
+    dest_files = map(lambda f: f.filename, get_all_music_files(dest))
+    # source_files = map(lambda f: find_source_file(f, src, dest), dest_files)
+    return ((find_source_file(destfile, src, dest), destfile)
+            for destfile in dest_files)
 
 def copy_tags_recursive(srcdir,destdir):
     srcdir = os.path.realpath(srcdir)
     destdir = os.path.realpath(destdir)
     for pair in find_file_pairs(srcdir,destdir):
-        print """Copying from '%s' to '%s'""" % pair
+        logger.info("""Copying tags from '%s' to '%s'""" % pair)
         copy_tags(*pair)
 
 def copy_tags (src, dest):
-    m_src = AudioFile(src, blacklist = blacklist_regexes)
-    m_dest = AudioFile(dest, blacklist = m_src.blacklist)
-    m_dest.clear()
-    m_dest.update(m_src)
-    m_dest.write()
+    """Replace tags of dest file with those of src.
+
+Excludes format-specific tags and replaygain info, which does not
+carry across formats."""
+    try:
+        m_src = AudioFile(src, blacklist = blacklist_regexes, easy=True)
+        m_dest = AudioFile(dest, blacklist = m_src.blacklist, easy=True)
+        m_dest.clear()
+        logger.debug("Adding tags from source file:\n%s",
+                      "\n".join("%s: %s" % (k, repr(m_src[k])) for k in sorted(m_src.keys())))
+        m_dest.update(m_src)
+        logger.debug("Added tags to dest file:\n%s",
+                     "\n".join("%s: %s" % (k, repr(m_dest[k])) for k in sorted(m_dest.keys())))
+        m_dest.write()
+    except AACError:
+        logger.warn("No tags copied because output format does not support tags: %s", repr(type(m_dest.data)))
 
 if __name__ == '__main__':
     if len(sys.argv[1:]) == 0:
-        print "No files specified."
+        logger.error("No files specified.")
+        sys.exit(1)
     if len(sys.argv[1:]) % 2 != 0:
-        print "Need an even number of files."
+        logger.error("Need an even number of files.")
+        sys.exit(2)
     file_pairs = dict(zip(sys.argv[1::2],sys.argv[2::2]))
-    for pair in file_pairs.iteritems():
+    for pair in file_pairs.items():
         pair = tuple(map(os.path.realpath, pair))
-        print """Copying tags from '%s' to '%s' """ % pair
         copy_tags_recursive(pair[0],pair[1])
